45bc53d66ef7d555b8bf44323a42732f87c56a6d
tubademir23 <tubademir23@gmail.com>
2019-10-02 11:47:26 +0300

    before material_ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {Container, Row, Col} from 'reactstrap'
+import login from './pages/login';
+import signup from './pages/signup';
+import home from './pages/home';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+          <Router>
+            <Switch>
+              <Route exact path="/" component={home}>
+              </Route>
+          
+              <Route exact path="/signup" component={signup}>
+              </Route>
+          
+              <Route exact path="/login" component={login}>
+              </Route>
+            </Switch>
+          </Router>
     </div>
   );
 }

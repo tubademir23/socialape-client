f81be26bacc6fca94c899373752593373b1c6091
tubademir23 <tubademir23@gmail.com>
2019-10-03 10:05:21 +0300

    theme color

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
-import ReactDOM from 'react-dom'
-
+// import ReactDOM from 'react-dom'
+//material-ui color tools
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import CreateMuiTheme from '@material-ui/core/styles/createMuiTheme';
 // import logo from './logo.svg';
 import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
 // import {Container, Row, Col} from 'reactstrap'
@@ -11,22 +13,41 @@ import Navbar from './components/Navbar';
   
 import './App.css';
  
+const theme = CreateMuiTheme({
+  palette: {
+    primary: {
+      light: '#56b5b8',
+      main: '#188588',
+      dark: '#00585b',
+      contrastText: '#ffffff',
+    },
+    secondary: {
+      light: '#ffffff',
+      main: '#e2f1f1',
+      dark: '#b0bebe',
+      contrastText: '#000000',
+    },
+  },
+});
 class App extends Component {
   render() {
 
     return (
-      <div className="App">
-        <Router>
-          <Navbar> </Navbar>
-            <div className="container">
-              <Switch>
-                <Route exact path="/" component={home}> </Route>
-                <Route path="/signup" component={signup}></Route>
-                <Route path="/login" component={login}></Route>
-              </Switch>
-          </div>
-        </Router>
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <Router>
+            <Navbar> </Navbar>
+              <div className="container">
+                <Switch>
+                  <Route exact path="/" component={home}> </Route>
+                  <Route path="/signup" component={signup}></Route>
+                  <Route path="/login" component={login}></Route>
+                </Switch>
+            </div>
+          </Router>
+        </div>
+      </MuiThemeProvider>
+      
     );
   }
 }
